461237fc35b9a963e2d9efcfb344e46d1a161c43
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-01-31 10:08:43 +0530

    Updates

---
+++ 784.030296782755d1c21ce3.js
@@ -0,0 +1,3 @@
+(self.webpackChunkdashboard=self.webpackChunkdashboard||[]).push([[784],{784:(e,n,a)=>{"use strict";a.r(n),a.d(n,{mount:()=>o});var r=a(212),d=a.n(r),s=a(468),t=a.n(s),l=a(599);const c=a.n(l)().h1`
+  padding: 16px;
+`,h=()=>d().createElement(c,null,"Dashboard Microfrontend"),o=e=>{t().render(d().createElement(h,null),e)}}}]);
