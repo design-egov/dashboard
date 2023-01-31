37a42ca02849cd3cfd0e17dc8d88b6a2ade12d29
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-01-31 10:34:16 +0530

    Updates

---
+++ 784.62f8c52d23478f41559f.js
@@ -0,0 +1,8 @@
+(self.webpackChunkdashboard=self.webpackChunkdashboard||[]).push([[784],{784:(e,i,t)=>{"use strict";t.r(i),t.d(i,{mount:()=>x});var r=t(212),a=t.n(r),d=t(468),h=t.n(d),n=t(599),s=t.n(n);s().h1`
+  padding: 16px;
+`;const p=s().div`
+    border-width: 0;
+    width: 100vw;
+    height: 100vh;
+    margin: -32px 0 0;
+  `,l=()=>a().createElement(p,null,a().createElement("iframe",{style:{width:"100%",height:"100vh",margin:"-30px 0px 0px 0px"},src:"https://ifix-qa.ifix.org.in/digit-ui/employee/dss/dashboard/ifix"})),x=e=>{h().render(a().createElement(l,null),e)}}}]);
